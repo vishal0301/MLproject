e9fb34711caa7d9740c1022a0a769570ff8f009a
Vishal0301 <vishalvaibhav000@gmail.com>
2023-04-05 12:16:21 +0530

    aws deployment congiguration file updated

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application=Flask(__name__)
+application=Flask(__name__, template_folder='src/templates')
 
 app=application
 
@@ -42,5 +42,5 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)        
+    app.run(host="0.0.0.0")        
 
